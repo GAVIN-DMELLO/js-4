44859f92bc9bc22317460d037766295dfb67b600
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-07-27 17:55:03 +0530

    add:objects

---
+++ 4.js
@@ -0,0 +1,66 @@
+// let arr = [1,2,3,4,5, "gavin"]
+// console.log(arr)
+
+// console.log(typeof arr)
+
+// arr[5].toUpperCase
+
+// console.log(arr)
+
+// let a= [10 , 20 , 30 , 40 , 50]
+
+// for(let i=0 ; i<1000; i++){
+//   a[i]=i;
+// }
+
+
+
+// console.log(a)
+
+// for(let i =0;i<1000;i++){
+//   a.unshift(69)
+// }
+// console.log(a)
+
+
+
+// for(let num of a){
+//   console.log(num)
+// }
+
+
+
+// let arr = []
+// let arr2 = arr
+
+// arr2.push(10,20,30)
+
+// console.log(arr)
+// console.log(arr2)
+
+
+// let a = [1,2,3,4,5]
+
+// console.log(a.splice(1,4))
+
+// console.log(a)
+
+// let arr = [1,2,3,4,]
+// let brr = [3,4,5]
+
+// let c = arr+brr
+
+
+let a = [3,2,6,4,7,10]
+// a.sort()
+// console.log(a)
+
+const[f,s,t] = a
+console.log(f,s,t)
+
+
+let b = ...a
+console.log(typeof b)
+
+
+
