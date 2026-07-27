bdca0ca08bf02d55fda636dd6f8791b4e65ba768
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-07-27 09:45:31 +0530

    add: number object

---
+++ index.js
@@ -0,0 +1,18 @@
+// console.log(5%2)
+
+// console.log(5**2)
+
+// console.log(5**3)
+
+let a = 10
+
+let b = 345.6821
+
+console.log(typeof b.toFixed(1))
+
+console.log(b.toPrecision(3))
+
+
+let otp = Math.floor(Math.random()*(9999-1000+1)+1000)
+console.log(otp)
+
