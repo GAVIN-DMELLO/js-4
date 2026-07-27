7a1d3a46c97c8dd1b7a8aece499e2e0722135ce2
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-07-27 15:10:49 +0530

    add: string and date

---
+++ 1.js
@@ -0,0 +1,11 @@
+const name = "Gavin"
+const fullName = "Gavin_Neel Dmello"
+const message = `Strike is coming soon`
+
+console.log(name , fullName , message)
+const string = "gofhe ff"
+console.log(string.length)
+
+console.log(fullName.slice(2,6))
+console.log(fullName.slice(-4
+))
