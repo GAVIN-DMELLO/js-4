d45a0153f67020a346b17dfd6b9cd4f4a98ddd32
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-07-29 23:32:40 +0530

    add:map

---
+++ map.js
@@ -0,0 +1,43 @@
+// const numbers = [2, 4, 6, 8, 10];
+// const multiplied = numbers.map((num)=>num*10)
+
+// console.log(multiplied)
+
+
+
+
+
+
+
+// const names = ["alice", "bob", "charlie", "david"];
+// const shoutingNames = names.map((name)=>name.toUpperCase())
+// console.log(shoutingNames)
+
+
+
+// const users = [
+//   { id: 1, name: "John", email: "john@test.com" },
+//   { id: 2, name: "Sarah", email: "sarah@test.com" },
+//   { id: 3, name: "Mike", email: "mike@test.com" }
+// ];
+
+// const emails = users.map((users)=>users.email)
+// console.log(emails)
+
+
+
+
+
+// const topMovies = ["The Matrix", "Inception", "Interstellar"];
+// const rankedMovies = topMovies.map((movie , index)=>`${index+1}. ${movie}`)
+// console.log(rankedMovies)
+
+
+
+
+
+
+// const tools = ["hammer", "screwdriver", "wrench"];
+
+// const inventory = tools.map((item)=>({item:item , inStock:true}))
+// console.log(inventory)
