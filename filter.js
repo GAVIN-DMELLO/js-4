c266f264212d15c30702b52cf048b97520007aa5
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-07-29 22:54:52 +0530

    add:filter method

---
+++ filter.js
@@ -0,0 +1,74 @@
+// const ages = [12, 18, 25, 8, 30, 15];
+
+// let adults=[];
+
+// adults = ages.filter((num)=>num>=18)
+
+// console.log(adults)
+
+
+
+// const animals = ["cat", "elephant", "dog", "giraffe", "fox"];
+
+// let longNames = []
+
+// longNames = animals.filter((animal)=>animal.length>3)
+
+// console.log(longNames)
+
+
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let even = []
+
+// even = numbers.filter((num)=>num%2===0)
+
+// console.log(even)
+
+// one doubt here that when i did only num%2 odd numebrs printed why ??
+
+
+
+
+
+
+
+
+
+
+// const tasks = [
+//   { id: 1, text: "Buy groceries", done: true },
+//   { id: 2, text: "Do laundry", done: false },
+//   { id: 3, text: "Study JavaScript", done: false },
+//   { id: 4, text: "Call Mom", done: true }
+// ];
+
+
+// let pendingTasks = []
+
+// pendingTasks= tasks.filter((task)=>task.done===false)
+
+// console.log(done)
+
+
+
+
+
+
+// const inventory = [
+//   { name: "Laptop", category: "Electronics", price: 1200 },
+//   { name: "T-Shirt", category: "Apparel", price: 20 },
+//   { name: "Headphones", category: "Electronics", price: 80 },
+//   { name: "Coffee Mug", category: "Home", price: 15 },
+//   { name: "Mouse", category: "Electronics", price: 45 }
+// ];
+
+
+// let affordableElectronics = []
+
+
+// affordableElectronics = inventory.filter((item)=>item.category==="Electronics" && item.price<=100)
+
+
+// console.log(affordableElectronics)
