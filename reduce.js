90f3ee7952da0c5c57cb76ae168a78b3ec8f525f
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-07-30 00:00:02 +0530

    add:reduce

---
+++ reduce.js
@@ -0,0 +1,62 @@
+// const numbers = [5, 10, 15, 20];
+
+// const total = numbers.reduce((acc,crr)=>{
+//   return acc+crr
+// },0)
+
+// console.log(total)
+
+
+
+// const words = ["apple", "banana", "cherry"];
+// const wordCount = words.reduce((acc,crr)=>{
+//   return acc+crr.length
+// },0)
+
+// console.log(wordCount)
+
+
+
+
+// const cart = [
+//   { item: "Book", price: 15 },
+//   { item: "Pen", price: 2 },
+//   { item: "Notebook", price: 5 }
+// ];
+// const totalCost = cart.reduce((acc,crr)=>{
+//   return acc+crr.price
+// },0)
+
+// console.log(totalCost)
+
+
+
+
+// const scores = [45, 89, 22, 100, 74];
+// const highestScore = scores.reduce((acc,crr)=>{
+//   if(crr>acc){
+//     return crr
+//   }else{
+//     return acc
+//   }
+// },0)
+// console.log(highestScore)
+
+
+
+
+// const votes = ["apple", "banana", "apple", "orange", "banana", "apple"];
+// const voteCount = votes.reduce((acc,crr)=>{
+//   if(crr==="apple"){
+//      acc.apple++;
+//   }
+//   else if(crr==="banana"){
+//      acc.banana++
+//   }
+//   else if(crr==="orange"){
+//      acc.orange++
+//   }
+
+//   return acc
+// },{apple:0,orange:0,banana:0})
+// console.log(voteCount)
