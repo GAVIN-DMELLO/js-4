389b1c9117189a1095fffcceb5de076fb5a4cdb7
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-07-30 22:42:16 +0530

    add: dom3 ex2

--- dom1.js
+++ dom1.js
@@ -1,5 +1,6 @@
-const todo = document.getElementById('todo-list')
-const li = document.createElement('li')
-li.textContent = "Finish DOM exercises"
-// todo.append(li)
-todo.insertAdjacentElement('beforeend',li)
+const feed = document.getElementById('feed')
+const div = document.createElement('div')
+div.classList.add('post')
+div.textContent = "Just woke up and learning DOM manipulation!"
+// feed.insertAdjacentElement('afterbegin',div)
+feed.prepend(div)
