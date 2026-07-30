8b4f477e59708b3b74fd1f8ce42a65d59c0e8555
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-07-30 22:37:45 +0530

    add:dom3 ex1

--- dom1.js
+++ dom1.js
@@ -1,18 +1,5 @@
-const userStatus = "success"; 
-
-const badge = document.getElementById('dynamic-badge')
-
-if(userStatus==="success"){
-  badge.classList.add('sucess-badge')
-  badge.style.backgroundColor='#d4edda'
-}else if(userStatus==="warning"){
-  badge.classList.add('warning-badge')
-  badge.style.backgroundColor='#fff3cd'
-}else if(userStatus==="danger"){
-  badge.classList.add('danger-badge')
-  badge.style.backgroundColor='#f8d7da'
-}
-
-badge.textContent = userStatus
-
-console.log(badge)
+const todo = document.getElementById('todo-list')
+const li = document.createElement('li')
+li.textContent = "Finish DOM exercises"
+// todo.append(li)
+todo.insertAdjacentElement('beforeend',li)
