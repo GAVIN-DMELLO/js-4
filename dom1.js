1b7492b3c344602701287bcd5e3576fde691bf5e
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-07-30 22:56:10 +0530

    add : dom3 ex5

--- dom1.js
+++ dom1.js
@@ -1,5 +1,11 @@
-const banner = document.getElementById('cookie-banner')
+const productGrid = document.getElementById('product-grid')
+const div = document.createElement('div')
+div.classList.add('product-card')
+const header = document.createElement('h3')
+const para = document.createElement('p')
 
-if(banner){
-  banner.remove()
-}
+header.textContent = "Wireless Headphones"
+para.textContent = "$99.00"
+
+productGrid.append(div)
+div.append(header , para)
