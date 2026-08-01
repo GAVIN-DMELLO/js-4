9f62ab96c46f931920e3c7efd0e21f51f5f8fa6d
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-08-02 00:06:48 +0530

    add:income tax calculator

--- income-tax-calculator/index.js
+++ income-tax-calculator/index.js
@@ -1,6 +1,5 @@
 const form = document.getElementById('form')
 const income = document.getElementById('income')
-const taxRate = document.getElementById('tax-rate')
 
 const grossIncome = document.getElementById('gross-income')
 const taxSlab = document.getElementById('res-tax-rate')
@@ -14,19 +13,36 @@ form.addEventListener('submit',(e)=>{
 })
 
 function displayTax(){
-  grossIncome.textContent = `Gross Income: ${income.value}`
+  grossIncome.textContent = ` ${income.value}`
 
-  taxSlab.textContent = `Tax Rate/Slab: ${taxRate.value}%`
+  taxSlab.textContent = `${calculateTax().taxRate}%`
 
-  payableTax.textContent = `Total Tax Payable: ${calculateTax()}`
+  payableTax.textContent = `${calculateTax().taxAmount}`
 
-  takeHome.textContent = `Net Take-Home Pay: ${income.value - calculateTax() }`
+  takeHome.textContent = `${income.value - calculateTax().taxAmount }`
 
-  effTaxRate.textContent = `Effective Tax Rate: ${taxRate.value}%`
+  effTaxRate.textContent = `${calculateTax().taxRate}%`
 
 }
 
 function calculateTax(){
-  const taxAmount = income.value *(taxRate.value/100)
+  let taxAmount = null
+  let additional = null
+  if(income.value>0 && income.value<1200000){
+    taxAmount = {taxAmount : 0 , taxRate:0}
+  }
+  else if(income.value<1600000){
+    taxAmount = {taxAmount : ((income.value-1200000) *(15/100)), taxRate:15}
+  }
+  else if(income.value<2000000){
+    taxAmount = {taxAmount : (((income.value-1200000)-400000)*(20/100) + 60000), taxRate:20}
+  }
+  else if(income.value < 2400000){
+    taxAmount = {taxAmount : ((((income.value-1200000)-800000))*(25/100) + 60000 + 80000), taxRate:25}
+  }
+  else if(income.value>2400000){
+    taxAmount = {taxAmount : ((income.value-2400000) *(30/100)+60000+80000+100000), taxRate:30}
+  }
   return taxAmount
+  console.log(taxAmount)
 }
