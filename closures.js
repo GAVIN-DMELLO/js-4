83898c61363896961a4439f87c328e65c81b8bee
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-08-06 11:44:22 +0530

    add : closures2

--- closures.js
+++ closures.js
@@ -11,40 +11,79 @@
 
 
 
-function createSequencer(steps , isLooping = false){
-  let i = 0
+// function createSequencer(steps , isLooping = false){
+//   let i = 0
+//   return{
+//     next:function(){
+//       if(!isLooping && i===steps.length-1){
+//         return steps[i]
+//       }
+//       if(isLooping && i===steps.length-1){
+//         i=0
+//         return steps[i]
+//       }
+//       i++
+//       return steps[i]
+//     },
+//     prev:function(){
+//       if(!isLooping && i===0){
+//         return steps[i]
+//       }
+//       if(isLooping && i===0){
+//         i=steps.length-1
+//         return steps[i]
+//       }
+//       i--
+//       return steps[i]
+//     },
+//     current:function(){
+//       return steps[i]
+//     },
+//     reset:function(){
+//       i=0
+//       return steps[i] 
+//     }
+//   }
+// }
+
+
+// console.log(createSequencer([1,2,3,4,5,6]).next())
+
+
+
+
+function createHistoryTracker(initialValue , maxHistory=5){
+  let history = []
+  let current = history.push(initialValue) - 1
+  console.log(current)
+  console.log(history)
+
   return{
-    next:function(){
-      if(!isLooping && i===steps.length-1){
-        return steps[i]
-      }
-      if(isLooping && i===steps.length-1){
-        i=0
-        return steps[i]
-      }
-      i++
-      return steps[i]
+    get:function(){
+      return history[current]
     },
-    prev:function(){
-      if(!isLooping && i===0){
-        return steps[i]
+    set:function(newValue){
+      if(history.length === maxHistory){
+        history.shift()
+        current = history.push(newValue) - 1
+        return current
       }
-      if(isLooping && i===0){
-        i=steps.length-1
-        return steps[i]
+    },
+    undo:function(){
+      if(current!=0){
+        return history[current--]
       }
-      i--
-      return steps[i]
     },
-    current:function(){
-      return steps[i]
+    redo:function(){
+      if(current != history.length - 1){
+        return history[current++]
+      }
     },
-    reset:function(){
-      i=0
-      return steps[i] 
+    getHistory:function(){
+      return {...history}
     }
   }
 }
 
-
-console.log(createSequencer([1,2,3,4,5,6]).next())
+const a = createHistoryTracker(2).get()
+console.count(typeof a)
