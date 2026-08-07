e033997764f8829c4abf40c22db8b0eb3fb96316
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-08-07 14:43:39 +0530

    add : react ch3

--- react1/index.js
+++ react1/index.js
@@ -1,14 +1,31 @@
-function UserProfile({name , role , isOnline}){
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>{role}</p>
-      <span>{isOnline?"Online":"Offline"}</span>
-    </div>
+// function UserProfile({name , role , isOnline}){
+//   return (
+//     <div>
+//       <h1>{name}</h1>
+//       <p>{role}</p>
+//       <span>{isOnline?"Online":"Offline"}</span>
+//     </div>
+//   )
+// }
+
+
+
+
+
+// const root = document.getElementById('root')
+
+// const rootEl = ReactDOM.createRoot(root)
+
+// rootEl.render(<UserProfile name="Gavin Neel Dmello" role="Frontend Developer" isOnline={true}/>)
+
+
+function SkillsList({skills=[]}){
+  return(
+    <ul>
+      {skills.map((item)=><li key={item}>{item}</li>)}
+    </ul>
   )
 }
-
-
 
 
 
@@ -16,4 +33,5 @@ const root = document.getElementById('root')
 
 const rootEl = ReactDOM.createRoot(root)
 
-rootEl.render(<UserProfile name="Gavin Neel Dmello" role="Frontend Developer" isOnline={true}/>)
+rootEl.render(<SkillsList skills={["git" , "js" , "nodejs" , "python"]}/>
+)
