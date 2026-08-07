1c079fa68ef7245ed335b27f6cc8448270186a7a
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-08-07 17:54:03 +0530

    add : react ch5

--- react1/index.js
+++ react1/index.js
@@ -63,3 +63,35 @@ function UserProfile({name , role , isOnline}){
 // const rootEl = ReactDOM.createRoot(root)
 
 // rootEl.render(<App />)
+
+const sampleProducts = [
+  { id: 101, name: "Wireless Mouse", price: 25, inStock: true },
+  { id: 102, name: "Mechanical Keyboard", price: 80, inStock: false },
+  { id: 103, name: "HD Monitor", price: 150, inStock: true }
+];
+
+
+
+function ProductList({products}){
+  return (
+    <ul>
+      {products.map((item)=>
+      {
+        return  item.inStock?
+        <li key={item.id}>{item.name}-${item.price} <span style={{color:'green'}}>[In Stock]</span> </li>
+        :
+        <li key={item.id} style={{ textDecoration: 'line-through', color: 'gray' }}>{item.name}-${item.price}
+        <span style={{color:'red' , textDecoration:'none'}}>[Out of Stock]</span></li>
+      })}
+    </ul>
+  )
+}
+
+
+
+ const root = document.getElementById('root')
+
+const rootEl = ReactDOM.createRoot(root)
+
+rootEl.render(<ProductList products={sampleProducts} />)
+
