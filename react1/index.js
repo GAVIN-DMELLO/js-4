7d591fd8e4dd8725aa42235e6389897cd4da2c1a
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-08-07 17:36:45 +0530

    add : react ch4

--- react1/index.js
+++ react1/index.js
@@ -1,12 +1,12 @@
-// function UserProfile({name , role , isOnline}){
-//   return (
-//     <div>
-//       <h1>{name}</h1>
-//       <p>{role}</p>
-//       <span>{isOnline?"Online":"Offline"}</span>
-//     </div>
-//   )
-// }
+function UserProfile({name , role , isOnline}){
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{role}</p>
+      <span>{isOnline?"Online":"Offline"}</span>
+    </div>
+  )
+}
 
 
 
@@ -19,19 +19,47 @@
 // rootEl.render(<UserProfile name="Gavin Neel Dmello" role="Frontend Developer" isOnline={true}/>)
 
 
-function SkillsList({skills=[]}){
-  return(
-    <ul>
-      {skills.map((item)=><li key={item}>{item}</li>)}
-    </ul>
-  )
-}
+// function SkillsList({skills=[]}){
+//   return(
+//     <ul>
+//       {skills.map((item)=><li key={item}>{item}</li>)}
+//     </ul>
+//   )
+// }
 
 
 
-const root = document.getElementById('root')
+// const root = document.getElementById('root')
 
-const rootEl = ReactDOM.createRoot(root)
+// const rootEl = ReactDOM.createRoot(root)
 
-rootEl.render(<SkillsList skills={["git" , "js" , "nodejs" , "python"]}/>
-)
+// rootEl.render(<SkillsList skills={["git" , "js" , "nodejs" , "python"]}/>
+// )
+
+
+
+// function Card({children}){
+//   return (
+//     <div style={{ border: '1px solid #ccc', padding: '16px', borderRadius: '8px' }}>
+//       {children}
+//     </div>
+//   )
+// }
+
+
+// function App(){
+//   return (
+//     <>
+//       <Card><UserProfile name="Gavin Neel Dmello" role="Frontend Developer" isOnline={true}/></Card>
+//       <Card><UserProfile name="Karan Aujla" role="Ghabru" isOnline={true}/></Card>
+//     </>
+//   )
+// }
+
+
+
+// const root = document.getElementById('root')
+
+// const rootEl = ReactDOM.createRoot(root)
+
+// rootEl.render(<App />)
